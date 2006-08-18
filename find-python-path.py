ffc7c751a76448614fa6cb608a648f77a761d9d0
dobey <dobey>
2006-08-18 20:27:57 +0000

    2006-08-18  Rodney Dawes  <dobey@novell.com>
    	* configure.ac: Determine the python site-packages path and substitute
    	it for the Makefiles to use
    
    	* find-python-path.py: Add this script to discover the python path
    
    	* python/Makefile.am: Remove the pythondir declaration from here, as
    	it is now defined by the configure script

---
+++ find-python-path.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+
+import sys
+from os.path import split, basename
+
+def find_path ():
+    for path in sys.path:
+	(head, tail) = split (path)
+	if tail == 'site-packages':
+	    base = basename (head)
+	    print base + '/' + tail
+	    return
+
+find_path ()
