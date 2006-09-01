9f0c26a8aa283bc95f6c68219891ccd508b798d7
dobey <dobey>
2006-09-01 19:44:04 +0000

    2006-09-01  Rodney Dawes  <dobey@novell.com>
    	* scripts/gnome-passwd.py: Run the right program here

--- scripts/gnome-passwd.py
+++ scripts/gnome-passwd.py
@@ -5,8 +5,8 @@ from ldtp import *
 from ldtputils import *
 from A11yTestUtils import *
 
-program_name = 'gnome-background-properties'
-window_title = 'Desktop Background Preferences'
+program_name = 'gnome-passwd'
+window_title = 'Change Password'
 
 a11y_test_init (program_name)
 
@@ -14,6 +14,6 @@ guiexist (window_title)
 
 a11y_scan_window (window_title)
 	
-click (window_title, 'btnClose')
+click (window_title, 'btnCancel')
 
 a11y_test_shutdown ()
