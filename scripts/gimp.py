d1055f01c09ec83576296dc487d4ce41f45cb8c8
dobey <dobey>
2006-09-18 20:22:45 +0000

    2006-09-18  Rodney Dawes  <dobey@novell.com>
    	* scripts/gimp.py: Update script to check more dialogs in GIMP, and
    	document a couple LDTP bugs that automating GIMP exposes

--- scripts/gimp.py
+++ scripts/gimp.py
@@ -31,8 +31,8 @@ a11y_scan_dialog ('dlgScreenShot', 'btnCancel')
 selectmenuitem (window_title, 'mnuXtns;mnuModuleManager')
 a11y_scan_dialog ('dlgModuleManager', 'btnClose')
 
-#selectmenuitem (window_title, 'mnuXtns;mnuPluginBrowser')
-#a11y_scan_dialog ('dlgPluginBrowser', 'btnClose')
+selectmenuitem (window_title, 'mnuXtns;mnuPlug-InBrowser')
+a11y_scan_dialog ('dlgPlug-InBrowser', 'btnClose')
 
 selectmenuitem (window_title, 'mnuXtns;mnuProcedureBrowser')
 a11y_scan_dialog ('dlgProcedureBrowser', 'btnClose')
@@ -40,13 +40,22 @@ a11y_scan_dialog ('dlgProcedureBrowser', 'btnClose')
 selectmenuitem (window_title, 'mnuXtns;mnuUnitEditor')
 a11y_scan_dialog ('dlgUnitEditor', 'btnClose')
 
+# LDTP BUG: Help and Help are the same, and LDTP uses a hash table...
 #selectmenuitem (window_title, 'mnuHelp;mnuHelp')
 #a11y_scan_dialog ('wndGIMPHelpBrowser', 'btnClose')
 
-#selectmenuitem (window_title, 'mnuHelp;mnuTipoftheDay')
-#a11y_scan_dialog ('dlgGIMPTipoftheDay', 'btnClose')
+selectmenuitem (window_title, 'mnuHelp;mnuTipoftheDay')
+a11y_scan_dialog ('frmGIMPTipoftheDay', 'btnClose')
 
-# Close GIMP
+selectmenuitem (window_title, 'mnuXtns;mnuScript-Fu;mnuButtons;mnuRoundButton')
+a11y_scan_dialog ('dlgScript-Fu:RoundButton', 'btnOK')
+
+# LDTP BUG: Need to wait for previous operations to complete
+# GIMP can't run multiple script-fu scripts at the same time
+#selectmenuitem (window_title, 'mnuXtns;mnuScript-Fu;mnuButtons;mnuSimpleBeveledButton')
+#a11y_scan_dialog ('dlgScript-Fu:SimpleBeveledButton', 'btnOK')
+
+# Close GIMP (Should wait for previous operations to complete)
 selectmenuitem (window_title, 'mnuFile;mnuQuit')
 
 a11y_test_shutdown ()
