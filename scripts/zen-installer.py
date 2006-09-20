4ef2e3b90b6005e9fdd39678a383cf43faa2390d
dobey <dobey>
2006-09-20 15:33:53 +0000

    2006-09-20  Rodney Dawes  <dobey@novell.com>
    	* scripts/zen-installer.py: Update to use the python module stuff for
    	scanning the program for accessibility issues

--- scripts/zen-installer.py
+++ scripts/zen-installer.py
@@ -3,51 +3,25 @@
 import string, sys, os
 from ldtp import *
 from ldtputils import *
-
-os.environ['GTK_MODULES'] = 'gail:atk-bridge'
-os.environ['GNOME_ACCESSIBILITY'] = '1'
+from A11yTestUtils import *
 
 program_name = 'zen-installer'
-window_title = '*Install*'
+window_title = 'Software Installer'
 
-def __has_label (windowName, componentName):
-    properties = getobjectinfo (windowName, componentName)
-    for prop in properties:
-	if prop == 'label':
-	    return 1
-	elif prop == 'label_by':
-	    return 1
-	continue
-    return 0
-
-def __print_properties (windowName, componentName):
-    properties = getobjectinfo (windowName, componentName)
-    for prop in properties:
-	propValue = getobjectproperty (windowName, componentName, prop)
-	print '\t', prop, ':', propValue
-
-def __check_dialog (windowName, closeButton):
-    objlist = getobjectlist (windowName)
-    has_label = 0
-    for obj in objlist:
-	if hasstate (windowName, obj, state.FOCUSABLE) == 1:
-	    if __has_label (windowName, obj) == 0:
-		print "WARNING:", obj, "has no accessible label defined"
-		__print_properties (windowName, obj)
-    click (windowName, closeButton)
-
-launchapp (program_name, 1)
+a11y_test_init (program_name)
 
 guiexist (window_title)
 
-objlist = getobjectlist (window_title)
+a11y_scan_window (window_title)
 
-has_label = 0
+click (window_title, 'btnConfigure')
+a11y_scan_window ('frmConfiguration')
+time.sleep (1)
+click ('frmConfiguration', 'btnAddService')
+time.sleep (1)
+#a11y_scan_dialog ('dlgAddService', 'btnCancel')
+click ('frmConfiguration', 'btnClose')
 
-for obj in objlist:
-    if hasstate (window_title, obj, state.FOCUSABLE) == 1:
-	if __has_label (window_title, obj) == 0:
-	    print "WARNING:", obj, "has no accessible label defined"
-	    __print_properties (window_title, obj)
-	
 click (window_title, 'btnClose')
+
+a11y_test_shutdown ()
